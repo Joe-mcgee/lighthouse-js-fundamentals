00f95b4d41a740d7cc3b10a22baa7e6bc8634fb0
Taylor <thulsmans.2994@me.com>
2017-12-01 00:50:46 +0000

    concat

---
+++ concat.js
@@ -0,0 +1,10 @@
+function concat(one, two) {
+  for (var i = 0; i < two.length; i++) {
+    one.push(two[i]);
+  }
+  return one;
+}
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
